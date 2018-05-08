25d6d5f60a7fedf9fa6dcf6ef223a33a66ec6e0a
Leonard Grazian <Grazian.Leonard@GMail.com>
2018-05-07 23:30:34 -0400

    better na fills, introduced ordinals

--- Model.py
+++ Model.py
@@ -5,31 +5,18 @@ import math
 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
-# from sklearn.linear_model import LinearRegression, Ridge
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+# from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_log_error
 
-
-def fill_na_by_type(X):
-    filled_values = X.select_dtypes(include='object').fillna('None')
-    for col in filled_values.columns:
-        X[col] = filled_values[col]
-    filled_values = X.select_dtypes(exclude='object').fillna(0)
-    for col in filled_values.columns:
-        X[col] = filled_values[col]
-    return X
+from eda.FillMissing import custom_fillna, custom_get_categoricals
 
 
-def reconcile_categoricals(X, X_test):
-    # get cols from X_test in case target is categorical
-    categorical_cols = X_test.select_dtypes(include='object').columns
-    for cat_col in categorical_cols:
-        all_categories = np.unique(
-                            np.append(np.unique(X[cat_col]),
-                                        np.unique(X_test[cat_col])) )
-        X[cat_col] = pd.Categorical(X[cat_col], categories=all_categories)
-        X_test[cat_col] = pd.Categorical(X_test[cat_col], categories=all_categories)
-    return X, X_test
+def make_submission(model, X_enc, y):
+    model.fit(X_enc, y)
+    predictions = model.predict(X_test_enc)
+    predictions_df = pd.DataFrame(data=predictions, index=X_test_enc.index, columns=['SalePrice'])
+    predictions_df.to_csv('output/submission_2.csv')
 
 
 train_data = pd.read_csv('data/train.csv', index_col=0)
@@ -39,9 +26,8 @@ X = fill_na_by_type( train_data.drop(columns=['SalePrice']) )
 test_data = pd.read_csv('data/test.csv', index_col=0)
 X_test = fill_na_by_type( test_data )
 
-X, X_test = reconcile_categoricals(X, X_test)
+X, X_test = custom_get_categoricals(X, X_test) # reconcile_categoricals(X, X_test)
 
-# TODO: each feature - should it be onehot? discrete but ordered? and much later, monotonic?
 # TODO: group features and train smaller models
 X_enc = pd.get_dummies(X, columns=X.select_dtypes(include='category').columns)
 X_test_enc = pd.get_dummies(X_test, columns=X_test.select_dtypes(include='category').columns)
@@ -51,7 +37,7 @@ X_test_enc = pd.get_dummies(X_test, columns=X_test.select_dtypes(include='catego
                                         y,
                                         random_state=42)
 
-model = RandomForestRegressor() # LinearRegression()
+model = Lasso() # RandomForestRegressor()
 model.fit(X_train, y_train)
 print(model.score(X_train, y_train))
 print(model.score(X_valid, y_valid))
@@ -59,8 +45,4 @@ print()
 print(math.sqrt(mean_squared_log_error(y_train, model.predict(X_train))))
 print(math.sqrt(mean_squared_log_error(y_valid, model.predict(X_valid))))
 
-# def make_submission(model, X_enc, y):
-model.fit(X_enc, y)
-predictions = model.predict(X_test_enc)
-predictions_df = pd.DataFrame(data=predictions, index=X_test_enc.index, columns=['SalePrice'])
-predictions_df.to_csv('output/submission_0.csv')
+make_submission(model, X_enc, y)
---
+++ eda/FillMissing.py
@@ -0,0 +1,119 @@
+
+import pandas as pd
+import numpy as np
+
+'''
+    LotFrontage: 13.83007039 + 0.58228893 * sqrt(LotArea)
+    Alley: 'None'
+    MasVnrType: 'None', except where MasVnrArea!=0, then it's 'BrkFace'
+    MasVnrArea: 0.0
+    BsmtQual: 'None' Ordinal(None, Fa, TA, Gd, Ex)
+    BsmtCond: 'None' Ordinal(Po, None, Fa, TA, Gd)
+    BsmtExposure: 'None' Ordinal(None, No, Mn, Av, Gd)
+    BsmtFinType1: 'None'
+    BsmtFinType2: 'None'
+    Electrical: 'None'
+    FireplaceQu: 'None' Ordinal(Po, None, Fa, TA, Gd, Ex)
+    GarageType: 'None'
+    GarageYrBlt: YearBuilt
+    GarageFinish: 'None'
+    GarageQual: 'None' Ordinal(Po, None, Fa, TA, Gd, Ex)
+    GarageCond: 'None' Ordinal(Po, None, Fa, TA, Gd, Ex)
+    PoolQC: 'None' Ordinal(None, Fa, Gd, Ex)
+    Fence: 'None'
+    MiscFeature: 'None'
+'''
+
+fillna_dict = {
+    'Alley': 'None',
+    'MasVnrType': 'None',
+    'MasVnrArea': 0.0,
+    'BsmtQual': 'None',
+    'BsmtCond': 'None',
+    'BsmtExposure': 'None',
+    'BsmtFinType1': 'None',
+    'BsmtFinType2': 'None',
+    'Electrical': 'None',
+    'FireplaceQu': 'None',
+    'GarageType': 'None',
+    'GarageFinish': 'None',
+    'GarageQual': 'None',
+    'GarageCond': 'None',
+    'PoolQC': 'None',
+    'Fence': 'None',
+    'MiscFeature': 'None'
+}
+
+
+cat_to_ord_maps = {
+    'BsmtQual':     {'None': 0, 'Fa': 1, 'TA': 2, 'Gd': 3, 'Ex': 4},
+    'BsmtCond':     {'Po': 0, 'None': 1, 'Fa': 2, 'TA': 3, 'Gd': 4},
+    'BsmtExposure': {'None': 0, 'No': 1, 'Mn': 2, 'Av': 3, 'Gd': 4},
+    'FireplaceQu':  {'Po': 0, 'None': 1, 'Fa': 2, 'TA': 3, 'Gd': 4, 'Ex': 5},
+    'GarageQual':   {'Po': 0, 'None': 1, 'Fa': 2, 'TA': 3, 'Gd': 4, 'Ex': 5},
+    'GarageCond':   {'Po': 0, 'None': 1, 'Fa': 2, 'TA': 3, 'Gd': 4, 'Ex': 5},
+    'PoolQC':       {'None': 0, 'Fa': 1, 'Gd': 2, 'Ex': 3}
+}
+
+def custom_fillna(X):
+    for col in X.columns:
+        if col in fillna_dict:
+            X[col] = X[col].fillna(fillna_dict[col])
+
+        if col == 'LotFrontage':
+            X[col] = X[col].fillna(13.83007039 + 0.58228893 * np.sqrt(X['LotArea']))
+
+        if col == 'GarageYrBlt':
+            X[col] = X[col].fillna(X['YearBuilt'])
+
+        if np.any(pd.isna(X[col])):
+            if X[col].dtype == 'object':
+                X[col] = X[col].fillna('None')
+            else:
+                X[col] = X[col].fillna(X[col].mode())
+
+    return X
+
+
+def custom_get_categoricals(X, X_test):
+    categorical_cols = X_test.select_dtypes(include='object').columns
+    for cat_col in categorical_cols:
+        if cat_col in cat_to_ord_maps:
+            X[cat_col] = X[cat_col].replace(cat_to_ord_maps[cat_col])
+            X_test[cat_col] = X_test[cat_col].replace(cat_to_ord_maps[cat_col])
+        else:
+            all_categories = np.unique(
+                                np.append(np.unique(X[cat_col]),
+                                            np.unique(X_test[cat_col])) )
+            X[cat_col] = pd.Categorical(X[cat_col], categories=all_categories)
+            X_test[cat_col] = pd.Categorical(X_test[cat_col], categories=all_categories)
+    return X, X_test
+
+
+def fill_na_by_type(X):
+    filled_values = X.select_dtypes(include='object').fillna('None')
+    for col in filled_values.columns:
+        X[col] = filled_values[col]
+    filled_values = X.select_dtypes(exclude='object').fillna(0)
+    for col in filled_values.columns:
+        X[col] = filled_values[col]
+    return X
+
+
+def reconcile_categoricals(X, X_test):
+    # get cols from X_test in case target is categorical
+    categorical_cols = X_test.select_dtypes(include='object').columns
+    for cat_col in categorical_cols:
+        all_categories = np.unique(
+                            np.append(np.unique(X[cat_col]),
+                                        np.unique(X_test[cat_col])) )
+        X[cat_col] = pd.Categorical(X[cat_col], categories=all_categories)
+        X_test[cat_col] = pd.Categorical(X_test[cat_col], categories=all_categories)
+    return X, X_test
+
+    
+if __name__ == '__main__':
+    root_dir = '/home/leonard/Desktop/Workspace/Kaggle/HousingPrices'
+    train_data = pd.read_csv('{}/data/train.csv'.format(root_dir), index_col=0)
+    train_data = custom_fillna(train_data)
+    print(train_data.head())
